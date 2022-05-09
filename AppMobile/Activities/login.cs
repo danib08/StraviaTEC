b2670c193f53980dddf9b4136a1f7b84280964d4
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2022-05-09 01:56:35 -0600

    Primera versión del app mobile

---
+++ AppMobile/Activities/login.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace AppMobile.Activities{
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+    public class login : AppCompatActivity{
+
+        private EditText editTextIdIn;
+        private EditText editTextPassIn;
+        private Button sendSignIn;
+        protected override void OnCreate(Bundle savedInstanceState){
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.login);
+
+            editTextIdIn = FindViewById<EditText>(Resource.Id.editTextIdIn);
+            editTextPassIn = FindViewById<EditText>(Resource.Id.editTextPassIn);
+            sendSignIn = FindViewById<Button>(Resource.Id.sendSignIn);           
+        
+        }
+    }
+}
---
+++ AppMobile/Activities/register.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace AppMobile.Activities{
+
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = false)]
+    public class register : Activity{
+
+        private EditText editTextId;
+        private EditText editTextPass;
+        private EditText editTextName;
+        private EditText editTextLastName;
+        private EditText editTextbirthDate;
+        private EditText editTextNationality;
+
+        private Button sendSignUp;
+
+        protected override void OnCreate(Bundle savedInstanceState){
+            base.OnCreate(savedInstanceState);
+            
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.register);
+            
+        }
+    }
+}
