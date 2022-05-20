c86969033258684e063398006d829a9f5e8594c1
gabrielgh123 <37197284+gabogh123@users.noreply.github.com>
2022-05-20 02:44:39 -0600

    MongoDB API creation

---
+++ API/StraviaMongo/MongoAPI/Models/StraviaTECSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoAPI.Models
+{
+    public class StraviaTECSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string ActivityCollectionName { get; set; } = null!;
+
+    }
+}
