8cdb797ae18f4f152c902a1126c217abc2bba6ae
gabrielgh123 <37197284+gabogh123@users.noreply.github.com>
2022-05-21 11:59:54 -0600

    Mongo changes

---
+++ API/StraviaMongo/StraviaMongo/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StraviaMongo.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [BsonElement("ActivityID")]
+        public string ActivityID { get; set; }
+        [BsonElement("AthleteID")]
+        public string AthleteID { get; set; }
+        [BsonElement("Text")]
+        public string Text { get; set; }
+    }
+}
